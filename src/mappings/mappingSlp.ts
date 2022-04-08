b08d64b060ecaacb24ec617417e42946b9438292
killbond007 <chenchengalex2014@gmail.com>
2022-04-08 21:00:32 +0200

    Initial commit

---
+++ src/mappings/mappingSlp.ts
@@ -0,0 +1,38 @@
+import {
+  SubstrateBlock,
+  SubstrateEvent,
+} from "@subql/types";
+import {  BlockNumber } from "@polkadot/types/interfaces";
+import { Compact } from "@polkadot/types";
+
+import {SlpInfo} from "../types";
+
+export async function handleSlp(block: SubstrateBlock): Promise<void> {
+  const blockNumber = (
+    block.block.header.number as Compact<BlockNumber>
+  ).toBigInt();
+  const slpEvents = block.events.filter(
+    (e) => e.event.section === "slp"
+  ) as SubstrateEvent[];
+
+  const era=await api.query.vtokenMinting.ongoingTimeUnit({
+    Token: 'KSM',
+  });
+
+  for (let slpEvent of slpEvents) {
+    const {
+      event: { data, method },
+    } = slpEvent;
+    const record = new SlpInfo(
+      blockNumber.toString() + "-" + slpEvent.idx.toString()
+    );
+
+    record.block_height = blockNumber;
+    record.block_timestamp = block.timestamp;
+    record.method = method.toString();
+    record.data = data.toString();
+    record.era = era.toString();
+
+    await record.save();
+  }
+}
