20e4ec3813984543177c2ef740f6fe0311a2ec4a
cheng <chenchengalex2014@gmail.com>
2022-10-31 14:16:27 +0100

    feat: add dot poolInfos

--- src/mappings/mappingSlp.ts
+++ src/mappings/mappingSlp.ts
@@ -4,8 +4,9 @@ import {
 } from "@subql/types";
 import { BlockNumber } from "@polkadot/types/interfaces";
 import { Compact } from "@polkadot/types";
+import BigNumber from "bignumber.js";
 
-import { SlpInfo } from "../types";
+import { SlpInfo, TokenStatus, FarmingBalance } from "../types";
 
 export async function handleSlp(block: SubstrateBlock): Promise<void> {
     const blockNumber = (
@@ -29,5 +30,100 @@ export async function handleSlp(block: SubstrateBlock): Promise<void> {
         record.data = data.toString();
 
         await record.save();
+    }
+
+    const status = await api.query.systemStaking?.tokenStatus({ "Token2": "0" });
+
+    const tokenStatus = JSON.parse(JSON.stringify(status))
+
+    const tokenStatusRecord = new TokenStatus(
+        blockNumber.toString()
+    );
+
+    tokenStatusRecord.block_height = blockNumber;
+    tokenStatusRecord.block_timestamp = block.timestamp;
+    tokenStatusRecord.farming_staking_amount = tokenStatus?.farmingStakingAmount ? BigInt(tokenStatus?.farmingStakingAmount) : BigInt(0);
+    tokenStatusRecord.system_stakable_amount = tokenStatus?.systemStakableAmount ? BigInt(tokenStatus?.systemStakableAmount) : BigInt(0);
+    tokenStatusRecord.system_shadow_amount = tokenStatus?.systemShadowAmount ? BigInt(tokenStatus?.systemShadowAmount) : BigInt(0);
+
+
+    await tokenStatusRecord.save();
+
+    const poolInfos = await api.query.farming?.poolInfos.entries();
+    await Promise.all(
+        poolInfos.map(async (item) => {
+            const poolId = item[0].toHuman()[0];
+            const poolInfo = JSON.parse(JSON.stringify(item[1]))
+            const rewardIssuer = poolInfo?.rewardIssuer;
+            const rewards = poolInfo?.rewards;
+            const tokensProportion = JSON.stringify(Object.keys(poolInfo.tokensProportion)[0])
+
+            await Promise.all(
+                Object.keys(rewards).map(async (key, index) => {
+                    const token = JSON.stringify(key)
+                    let token_free
+
+                    if (token.includes('BNC')) {
+                        const nativeBalance = await api.query.system.account(rewardIssuer);
+                        token_free = JSON.parse(JSON.stringify(nativeBalance))?.data?.free
+                    } else {
+                        const tokenBalance = await api.query.tokens.accounts(rewardIssuer, renderToken(token).token);
+                        token_free = JSON.parse(JSON.stringify(tokenBalance))?.free
+                    }
+
+                    const farmingBalanceRecord = new FarmingBalance(
+                        blockNumber.toString() + "-" + poolId.toString() + "-" + index.toString()
+                    );
+                    const claim_remain = new BigNumber(rewards[key][0])
+                        .minus(rewards[key][1])
+                        .toNumber()
+
+                    farmingBalanceRecord.block_height = blockNumber;
+                    farmingBalanceRecord.block_timestamp = block.timestamp;
+                    farmingBalanceRecord.tokens_proportion = tokensProportion
+                    farmingBalanceRecord.pool_name = renderToken(tokensProportion).label
+                    farmingBalanceRecord.reward_issuer = rewardIssuer;
+                    farmingBalanceRecord.pool_token = poolId + '-' + renderToken(tokensProportion).label + '-' + renderToken(token).label;
+                    farmingBalanceRecord.pool_id = poolId;
+                    farmingBalanceRecord.token = token;
+                    farmingBalanceRecord.token_decimals = renderToken(token).decimals;
+                    farmingBalanceRecord.token_free = token_free ? BigInt(token_free) : BigInt(0);
+                    farmingBalanceRecord.claim_remain = BigInt(claim_remain);
+                    farmingBalanceRecord.remain = BigInt(new BigNumber(token_free)
+                        .minus(claim_remain)
+                        .toNumber());
+
+                    await farmingBalanceRecord.save();
+                }))
+        }))
+
+}
+
+const renderToken = (key) => {
+    switch (true) {
+        case key === '"{\\"token\\":\\"ZLK\\"}"':
+            return { label: 'ZLK', token: { 'Token': 'ZLK' }, decimals: BigInt('1000000000000000000') }
+        case key === '"{\\"native\\":\\"BNC\\"}"':
+            return { label: 'BNC', token: { 'Native': 'BNC' }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"vToken\\":\\"0\\"}"':
+            return { label: 'vDOT', token: { 'VToken': '0' }, decimals: BigInt('10000000000') }
+        case key === '"{\\"lpToken\\":[\\"KSM\\",1,\\"KSM\\",2]}"':
+            return { label: 'LP vDOT-DOT', token: { 'LPToken': ['KSM', 1, 'KSM', 2] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"KSM\\",2,\\"KSM\\",4]}"':
+            return { label: 'LP vsKSM-KSM', token: { 'LPToken': ['KSM', 2, 'KSM', 4] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"ASG\\",0,\\"KSM\\",2]}"':
+            return { label: 'LP BNC-KSM', token: { 'LPToken': ['ASG', 0, 'KSM', 2] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"KSM\\",2,\\"KUSD\\",3]}"':
+            return { label: 'LP KSM-aUSD', token: { 'LPToken': ['KSM', 2, 'KUSD', 3] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"ASG\\",0,\\"ZLK\\",2]}"':
+            return { label: 'LP BNC-ZLK', token: { 'LPToken': ['ASG', 0, 'ZLK', 2] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"KAR\\",2,\\"ZLK\\",2]}"':
+            return { label: 'LP KAR-ZLK', token: { 'LPToken': ['KAR', 2, 'ZLK', 2] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"KSM\\",2,\\"RMRK\\",2]}"':
+            return { label: 'LP KSM-RMRK', token: { 'LPToken': ['KSM', 2, 'RMRK', 2] }, decimals: BigInt('1000000000000') }
+        case key === '"{\\"lpToken\\":[\\"MOVR\\",1,\\"MOVR\\",2]}"':
+            return { label: 'LP vMOVR-MOVR', token: { 'LPToken': ['MOVR', 1, 'MOVR', 2] }, decimals: BigInt('1000000000000') }
+        default:
+            return { label: 'ZLK', token: { 'Token': 'ZLK' }, decimals: BigInt('1000000000000000000') }
     }
 }
